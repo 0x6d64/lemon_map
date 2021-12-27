6621ecbffc3ffb57012f37358965eb2394cc2ea4
Martin Demling <0x6d64@martin-demling.de>
2021-12-27 23:12:33 +0200

    add config module

---
+++ lemon_map/config.py
@@ -0,0 +1,45 @@
+import configparser
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class LemonConfig(configparser.SafeConfigParser):
+    DEFAULT_CONFIG_FILE = os.path.join(os.path.expanduser("~"), ".lemonmaprc")
+
+    def __init__(self):
+        super().__init__()
+        self.file_path = None
+
+    @classmethod
+    def from_file(cls, filename=None):
+        if not filename:
+            filename = cls.DEFAULT_CONFIG_FILE
+
+        if not os.path.exists(filename):
+            logger.warning("file does not exist, loading default config: %s", filename)
+            new_config = LemonConfig.get_default_config()
+        else:
+            new_config = cls()
+            new_config.read(filename)
+        new_config.file_path = filename
+        return new_config
+
+    @classmethod
+    def get_default_config(cls):
+        default_config = cls()
+        default_config["DEFAULT"] = {
+            "auth_file": "~/.lemon_map.auth",
+        }
+        return default_config
+
+    def save_to_file(self, filename=None):
+        if filename:
+            destination_file = filename
+        elif self.file_path:
+            destination_file = self.file_path
+        else:
+            destination_file = self.DEFAULT_CONFIG_FILE
+        with open(destination_file, "w") as c_fp:
+            self.write(c_fp)
