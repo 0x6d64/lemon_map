ddf3c795d09898a39152a332aae1e178dceee35c
Martin Demling <0x6d64@martin-demling.de>
2021-12-29 22:51:35 +0200

    apply black

--- lemon_map/vehicles.py
+++ lemon_map/vehicles.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
-class Vehicle():
+
+class Vehicle:
     def __init__(self):
         # TODO
         pass
 
+
 class Scooter(Vehicle):
     pass
+
 
 class NonScooter(Vehicle):
     pass
 
-class VehicleStats():
+
+class VehicleStats:
     def __init__(self):
         # TODO
         pass
